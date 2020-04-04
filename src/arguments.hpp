51bc5a16410e0cde24352469cf6b36fabe5e3833
Bruno Borchardt <bruno.borchardt@tuhh.de>
2020-04-04 19:51:52 +0200

    virtual constexpr auskommentiert vorraussichtlich letzte version ohne module

---
+++ src/arguments.hpp
@@ -0,0 +1,61 @@
+#pragma once	//NOLINT
+
+#include "baseTerm.hpp"
+
+#include <string>
+#include <cassert>
+
+namespace bmath::intern {
+
+
+
+	template<Base_Type base_type>
+	class Value final : public Base_Term<base_type>, public std::complex<double>
+	{
+	public:
+		Value(double re, double im) :std::complex<double>(re, im) {}
+		virtual ~Value() {} //no pointers to other terms are owned -> nothing to do here
+
+		virtual void to_str(std::string& str, int caller_operator_precedence) const override { str.append("value"); }
+
+		virtual /*constexpr*/ Derived_Type get_type() const override { return Derived_Type::value; }
+
+		static constexpr Value<base_type>* down_cast(Base_Term<base_type>* base_ptr)
+			{ assert(base_ptr->get_type() == Derived_Type::value); return static_cast<Value<base_type>*>(base_ptr); }
+	};
+
+	using Regular_Value = Value<Base_Type::regular>;
+	using Pattern_Value = Value<Base_Type::pattern>;
+	static_assert(!std::is_same<Regular_Value, Pattern_Value>::value);	//the reason to use templates with Base_Type is to create different types.
+
+
+	class Regular_Variable final : public Regular_Term
+	{
+	public:
+		const std::string name;
+
+		virtual void to_str(std::string& str, int caller_operator_precedence) const override { str.append(this->name); }
+
+		virtual /*constexpr*/ Derived_Type get_type() const override { return Derived_Type::variable; }
+
+		static /*constexpr*/ Regular_Variable* down_cast(Regular_Term* base_ptr)
+			{ assert(base_ptr->get_type() == Derived_Type::variable); return static_cast<Regular_Variable*>(base_ptr); }
+	};
+
+	class Pattern_Variable final : public Pattern_Term
+	{
+	public:
+		const std::string name;
+
+		Regular_Term* matched_term;
+
+		virtual void to_str(std::string& str, int caller_operator_precedence) const override { str.append('{' + this->name + '}'); }
+
+		virtual /*constexpr*/ Derived_Type get_type() const override { return Derived_Type::variable; }
+
+		static /*constexpr*/ Pattern_Variable* down_cast(Pattern_Term* base_ptr)
+			{ assert(base_ptr->get_type() == Derived_Type::variable); return static_cast<Pattern_Variable*>(base_ptr); }
+
+	};
+
+} //namespace bmath::intern
