e4035e06d82b3dae1f21b5aa7b793122e8769eef
Bruno <46483121+brunizzl@users.noreply.github.com>
2020-09-02 17:04:06 +0200

    dämliche main aus dämlicher gitignore genommen

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include "visit.hpp"
+#include "internalFunctions.hpp"
+#include "termStore.hpp"
+
+using namespace bmath::intern;
+
+
+int main()
+{
+    Regular_Power* term = new Regular_Power(new Regular_F1(F1_Type::sin, new Regular_Value({ 3.1415/4, 0 })), new Regular_Value({ 2, 0 }));
+	std::cout <<
+		regular_visit(term,
+			[](Regular_F1*) { return "par"; },
+			[](Regular_Log*) { return "log"; },
+			[](Regular_Power*) { return "pow"; },
+			[](Regular_Product*) { return "pro"; },
+			[](Regular_Sum*) { return "sum"; },
+			[](Regular_Value*) { return "val"; },
+			[](Regular_Variable*) { return "var"; }) << std::endl;
+	std::cout << evaluate(term) << std::endl;
+    delete term;
+
+    std::cin.get();
+}
