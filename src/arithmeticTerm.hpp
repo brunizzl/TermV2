4f8ec46e72027910ae4c69acbb0d3e844f2deeb5
Bruno <46483121+brunizzl@users.noreply.github.com>
2020-09-26 23:53:41 +0200

    template instanziierungen minimiert, indem ToProduct und ToString nur noch alias für ToIndexSLC sind

--- src/arithmeticTerm.hpp
+++ src/arithmeticTerm.hpp
@@ -34,27 +34,27 @@ namespace bmath::intern::arithmetic {
 
 	enum class FunctionType : std::uint32_t
 	{
-		asinh,	//argument := param_1
-		acosh,	//argument := param_1
-		atanh,	//argument := param_1
-		asin,	//argument := param_1
-		acos,	//argument := param_1
-		atan,	//argument := param_1
-		sinh,	//argument := param_1
-		cosh,	//argument := param_1
-		tanh,	//argument := param_1
-		sqrt,	//argument := param_1
-		pow,    //base     := param_1  expo     := param_2
-		log,	//base     := param_1  argument := param_2
-		exp,	//argument := param_1
-		sin,	//argument := param_1
-		cos,	//argument := param_1
-		tan,	//argument := param_1
-		abs,	//argument := param_1
-		arg,	//argument := param_1
-		ln,		//argument := param_1
-		re,		//argument := param_1
-		im,		//argument := param_1
+		asinh,	//param_1 := argument
+		acosh,	//param_1 := argument
+		atanh,	//param_1 := argument
+		asin,	//param_1 := argument
+		acos,	//param_1 := argument
+		atan,	//param_1 := argument
+		sinh,	//param_1 := argument
+		cosh,	//param_1 := argument
+		tanh,	//param_1 := argument
+		sqrt,	//param_1 := argument
+		pow,    //param_1 := base      param_2 := expo    
+		log,	//param_1 := base      param_2 := argument
+		exp,	//param_1 := argument
+		sin,	//param_1 := argument
+		cos,	//param_1 := argument
+		tan,	//param_1 := argument
+		abs,	//param_1 := argument
+		arg,	//param_1 := argument
+		ln,		//param_1 := argument
+		re,		//param_1 := argument
+		im,		//param_1 := argument
 		COUNT	//has to be last element
 	};
 
@@ -70,8 +70,8 @@ namespace bmath::intern::arithmetic {
 
 	struct UnknownFunction
 	{
-		enum class ParamCount :char { one, two, more } param_count : 4;
-		enum class NameSize :char { small, longer } name_size : 4;
+		enum class ParamCount :unsigned char { one, two, more } param_count : 4;
+		enum class NameSize :unsigned char { small, longer } name_size : 4;
 
 		char short_name[3];	//can be used as short_name[7], if short_name_extension is active member
 		union
@@ -81,14 +81,9 @@ namespace bmath::intern::arithmetic {
 		};
 		union
 		{
-			std::uint32_t long_param_colony_index; //points to UnknownFunction_Parameters containing ALL parameters (if active)
+			std::uint32_t long_param_colony_index; //points to TypedIdxColony containing ALL parameters (if active)
 			TypedIdx short_parameters[2];
 		};
-	};
-
-	struct UnknownFunction_Parameters :TypedIdxColony
-	{
-		using TypedIdxColony::TypedIdxColony;
 	};
 
 	struct Variable :TermString128
@@ -100,37 +95,35 @@ namespace bmath::intern::arithmetic {
 
 	union TypesUnion
 	{
-		Sum sum;
-		Product product;
 		KnownFunction known_function;
 		UnknownFunction unknown_function;
 		Variable variable;
 		Complex complex;
 		TermString128 string;	//Variable and UnknownFunction may allocate additional string nodes
-		TypedIdxColony typedidx_col;	//UnknownFunction may allocate additional colony nodes. 
+		TypedIdxColony index_slc; //representing Sum, Product or UnknownFunction's extra parameters 
 
 		TypesUnion() :complex() {}
 
-		TypesUnion(const Sum &            val) :sum(val)              {}
-		TypesUnion(const Product&         val) :product(val)          {}
 		TypesUnion(const KnownFunction&   val) :known_function(val)   {}
 		TypesUnion(const UnknownFunction& val) :unknown_function(val) {}
 		TypesUnion(const Variable&        val) :variable(val)         {}
 		TypesUnion(const Complex&         val) :complex(val)          {}
 		TypesUnion(const TermString128&   val) :string(val)           {} 
-		TypesUnion(const TypedIdxColony&  val) :typedidx_col(val)     {}
+		TypesUnion(const TypedIdxColony&  val) :index_slc(val)        {}
 	};
 
 	static_assert(sizeof(TypesUnion) * 8 == 128);
 	using ArithmeticStore = TermStore<TypesUnion>;
 
 	//everything using the TermSLC needs to have a way to get the right union member from the union
-	struct ToSum     { using Result = Sum;	         static Result& apply(TypesUnion& val) { return val.sum;     } };
-	struct ToProduct { using Result = Product;       static Result& apply(TypesUnion& val) { return val.product; } };
-	struct ToString  { using Result = TermString128; static Result& apply(TypesUnion& val) { return val.string;  } };
-	struct ToConstSum     { using Result = const Sum;           static const Result& apply(const TypesUnion& val) { return val.sum;     } };
-	struct ToConstProduct { using Result = const Product;       static const Result& apply(const TypesUnion& val) { return val.product; } };
-	struct ToConstString  { using Result = const TermString128;	static const Result& apply(const TypesUnion& val) { return val.string;  } };
+	struct ToString   { using Result = TermString128;  static Result& apply(TypesUnion& val) { return val.string;    } };
+	struct ToIndexSLC { using Result = TypedIdxColony; static Result& apply(TypesUnion& val) { return val.index_slc; } };
+	struct ToConstString   { using Result = const TermString128;  static Result& apply(const TypesUnion& val) { return val.string;    } };
+	struct ToConstIndexSLC { using Result = const TypedIdxColony; static Result& apply(const TypesUnion& val) { return val.index_slc; } };
+	using ToSum     = ToIndexSLC;
+	using ToProduct = ToIndexSLC;
+	using ToConstSum     = ToConstIndexSLC;
+	using ToConstProduct = ToConstIndexSLC;
 
 	//evaluates tree if possible, throws if variables of unknown value /unknown_functions are present
 	std::complex<double> eval(const ArithmeticStore& store, TypedIdx ref);
