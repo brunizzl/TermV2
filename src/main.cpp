9d75b9b5a0ebc26244de7732f4d81b04e42c2e37
Bruno Borchardt <bruno.borchardt@tuhh.de>
2020-11-13 09:58:39 +0100

    pattern constructor reihenfolge geändert , erlaubt bessere value match variablen

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,13 @@ int main()
 {
 	//test::combine_exact();
 	//test::arithmetic_term();
-	//test::pattern_term();
+	test::pattern_term();
 	//test::copy();
 	//test::bit_set();
 	//test::match();
 	//
 	//debug::enumerate_pn_type();
-	debug::test_rechner();
+	//debug::test_rechner();
 }
 
 
