4f37eb954cac0a0c0359e05a649169ed3cd0a21c
Bruno <46483121+brunizzl@users.noreply.github.com>
2021-05-27 10:12:52 +0200

    removed Queue::iter

--- src/utility/queue.hpp
+++ src/utility/queue.hpp
@@ -14,35 +14,6 @@ namespace simp {
 		static_assert(std::is_trivially_destructible_v<Value_T>);
 		static_assert(BufferSize > 0);
 
-		struct Iter
-		{
-			Value_T* pos;
-			Value_T* const start_data;
-			Value_T* const end_data;
-
-			//http://www.cplusplus.com/reference/iterator/RandomAccessIterator/
-			using value_type = Value_T;
-			using difference_type = std::ptrdiff_t;
-			using pointer = Value_T*;
-			using reference = Value_T&;
-			using iterator_category = std::forward_iterator_tag;
-
-			constexpr Iter& operator++() noexcept
-			{
-				if (++this->pos == this->end_data) [[unlikely]] {
-					this->pos = start_data;
-				}
-				return *this;
-			}
-
-			constexpr Iter operator++(int) noexcept { const Iter old = *this; this->operator++(); return old; }
-
-			constexpr Value_T& operator*() noexcept { return *this->pos; }
-			constexpr Value_T* operator->() noexcept { return this->pos; }
-
-			constexpr bool operator==(const Iter&) const noexcept = default;
-		}; //struct Iter
-
 		Value_T local_data[BufferSize];
 
 		Value_T* start_data;
@@ -55,10 +26,7 @@ namespace simp {
 
 		constexpr std::size_t capacity() const noexcept { return this->end_data - this->start_data; }
 
-		constexpr Iter make_iter(Value_T* const pos) noexcept { return { pos, this->start_data, this->end_data }; };
-
-		constexpr Iter begin() noexcept { return this->make_iter(this->start_used); }
-		constexpr Iter end()   noexcept { return this->make_iter(this->end_used); }
+		constexpr void incr(Value_T*& pos) const noexcept { if (++pos == this->end_data) [[unlikely]] { pos = this->start_data; } }
 
 		constexpr void unsave_change_capacity(const std::size_t new_cap) noexcept
 		{
@@ -66,10 +34,14 @@ namespace simp {
 			Value_T* const new_data = new Value_T[new_cap];
 
 			assert(this->size_ >= 1);
-			*new_data = *this->start_used;
-			//due to queues circular nature, this->begin() can start at end, but not be invalid.
+			//due to queues circular nature, from can start at this->end_used, but not be invalid.
 			//that case occurs precisely when this->capacity() == this->size_
-			std::copy(this->make_iter(this->start_used + 1), this->end(), new_data + 1); 
+			Value_T* from = this->start_used;
+			Value_T* to = new_data;
+			do {
+				*to = *from;
+				this->incr(from);
+			} while (from != this->end_used);
 
 			if (this->start_data != this->local_data) {
 				delete[] this->start_data;
@@ -79,7 +51,7 @@ namespace simp {
 			this->end_data = new_data + new_cap;
 
 			this->start_used = new_data;
-			this->end_used = new_data + this->size_;
+			this->end_used = to;
 		} //unsave_change_capacity
 
 	public:
@@ -111,9 +83,7 @@ namespace simp {
 
 			new (this->end_used) Value_T{ std::forward<Args>(args)... };
 
-			if (++this->end_used == this->end_data) [[unlikely]] {
-				this->end_used = this->start_data;
-			}
+			this->incr(this->end_used);
 		}
 
 		constexpr Value_T pop_front()
@@ -122,9 +92,7 @@ namespace simp {
 			--this->size_;
 
 			Value_T* const res_pos = this->start_used;
-			if (++this->start_used == this->end_data) [[unlikely]] {
-				this->start_used = this->start_data;
-			}
+			this->incr(this->start_used);
 			return *res_pos;
 		}
 	}; //class Queue
